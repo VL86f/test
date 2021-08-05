277a9bd6c6bcd7b2d8e025424c7a6be69e192f27
VL86f <and.itzkoff@yandex.ru>
2021-08-06 00:21:02 +0800

    writing Java code

--- testing/java/game.java
+++ testing/java/game.java
@@ -1,5 +1,25 @@
+import java.util.*;
 public class game {
 	public static void main(String[] args){
-		System.out.println("Welcome to Java (OpenJDK)");
+		Boolean test = false;
+		String clear = "\033[H\033[2J";
+		Scanner z = new Scanner(System.in);
+		while(test == false) {
+			System.out.println("Welcome to Java (OpenJDK)");
+			System.out.println("To find out the rule of the game, enter 1");
+			System.out.println("To start the game enter 2");
+			System.out.println("To exit the game enter 3");
+			switch (z.next()) {
+				case "3" :
+					test = true;
+				break;
+				case "2" :
+				
+				case "1" :
+				default:
+					System.out.print(clear);
+				break;
+			}
+		}		
 	}
 }
